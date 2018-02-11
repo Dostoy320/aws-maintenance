4606dfe565aaf513dc3c5592a18ef3eca050ec97
Adam Gregory <dostoy320@gmail.com>
2018-02-11 11:25:55 -0600

    Added RDS snapshot backup script

---
+++ create-rds-snapshot.py
@@ -0,0 +1,28 @@
+# 
+# Run this function with the following environment variables set to create manual RDS snapshots:
+# instances = [comma-separated string of rds instance names]
+# region = AWS region - ie: us-east-1
+#
+
+import botocore  
+import datetime  
+import re  
+import logging
+import boto3
+import os
+ 
+region= os.environ.get("region")
+instances = os.environ.get("instances").split(",")
+ 
+print('Loading function')
+ 
+def lambda_handler(event, context):  
+    source = boto3.client('rds', region_name=region)
+    for instance in instances:
+        try:
+            timestamp1 = str(datetime.datetime.now().strftime('%Y-%m-%d-%H-%-M-%S')) + "lambda-snap"
+            snapshot = "{0}-{1}-{2}".format("mysnapshot", instance,timestamp1)
+            response = source.create_db_snapshot(DBSnapshotIdentifier=snapshot, DBInstanceIdentifier=instance)
+            print(response)
+        except botocore.exceptions.ClientError as e:
+            raise Exception("Could not create snapshot: %s" % e)
